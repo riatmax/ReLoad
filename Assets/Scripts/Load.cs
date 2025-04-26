6fd580a57690e46595aba6c612c6ca90fe3e4858
riatmax <riatmax@gmail.com>
2025-04-26 10:40:19 -0400

    Update Load.cs

--- Assets/Scripts/Load.cs
+++ Assets/Scripts/Load.cs
@@ -7,17 +7,18 @@ public class Load : MonoBehaviour
 {
     [Header("Load Stats")]
     [SerializeField]
-    private int health;
+    private int health; // loading image health
     [SerializeField]
-    private float loadPercentage;
+    private float loadPercentage; // how much the bar is filled
     [SerializeField]
-    private float loadSpeedModifier;
+    private float loadSpeedModifier; // how fast bar fills
 
     [Header("Loading Bar")]
     public Slider healthSlider; 
 
     private void Update()
     {
+        // bar load logic
         loadPercentage += Time.deltaTime * loadSpeedModifier;
         loadPercentage = Mathf.Clamp(loadPercentage, 0, 100);
         healthSlider.value = loadPercentage/100;
